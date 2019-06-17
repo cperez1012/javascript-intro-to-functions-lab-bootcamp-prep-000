a36dca83dfa0de77e5c2383ecb3aaa4dc635b324
Christian Perez <perezchristian1012@gmail.com>
2019-06-17 17:52:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,12 +12,3 @@ function logWhisper(string) {
 console.log('hello')
 return string.toLowerCase()
 }
-function sayHiToGrandma(string) {
-return 'hello'.toLowerCase()
-}
-function sayHiToGrandma(string) {
-return 'HELLO'.toUpperCase()
-}
-function sayHiToGrandma(string) {
-return "I love you, Grandma."
-}
